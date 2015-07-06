c6790a9624442cf7e0f7fbba2a8af64f8fd7a866
Enrique Girón <egironmart@gmail.com>
2015-07-06 11:47:18 +0200

    Primeira páxina

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var cculturalController = require('../controllers/ccultural_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Compostela Cultural' });
 });
+
+router.get('/actos/acto', cculturalController.acto);
 
 module.exports = router;
